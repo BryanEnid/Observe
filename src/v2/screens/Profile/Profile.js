d86a225111bd94f8c15bf93019e351cf61c28754
brodriguez-eb <bryan.rodriguez@employbridge.com>
2022-06-30 17:41:14 -0400

    still working

--- src/v2/screens/Profile/Profile.js
+++ src/v2/screens/Profile/Profile.js
@@ -1,62 +1,75 @@
 /* eslint-disable */
 // https://medium.com/@linjunghsuan/implementing-a-collapsible-header-with-react-native-tab-view-24f15a685e07
 
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Header } from './Header';
+// import ConicalGradient from '../../components/ConicalGradient/ConicalGradient';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import { useDummyData } from '../../hooks/useDummyData';
+import { Text, Box, Pressable, VStack, Center, Image } from 'native-base';
 
-const HeaderHeight = 280;
-
-const profileMock = {
-  name: {
-    first: 'Bryan',
-    last: 'Tejada',
-  },
-  login: {
-    username: 'BryanEnid',
-  },
-};
+const profileSize = { width: 180, height: 180, padding: 20 };
 
 const styles = StyleSheet.create({
-  header: {
-    top: 0,
-    height: HeaderHeight,
-    width: '100%',
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
+  profile_picture: {
+    width: profileSize.width - profileSize.padding,
+    aspectRatio: 1,
+    borderRadius: profileSize.width - profileSize.padding / 2,
   },
-  label: { fontSize: 16, color: '#222' },
-  tab: { elevation: 0, shadowOpacity: 0, backgroundColor: 'white' },
-  indicator: { backgroundColor: '#222' },
 });
 
 export const Profile = () => {
+  // Hooks
   const { getRandomUsers, getRandomVideos } = useDummyData();
 
-  const [profile, setProfile] = useState();
-  const [videoURI, setVideo] = useState();
-  const [user] = useState({ quote: 'Seagulls are the eagles of the sea.' });
+  // State
+  const [profile, setProfile] = React.useState();
+  const [videoURI, setVideo] = React.useState();
+  const [user] = React.useState({ quote: 'Seagulls are the eagles of the sea.' });
 
-  // Fetching dummy data (Profile, and videos)
-  useEffect(() => {
+  // Life cycle
+  React.useEffect(() => {
+    // Fetching dummy data (Profile, and videos)
     getRandomUsers(1).then(({ results }) => setProfile(results[0]));
-
     const randomNumber = Math.round(Math.random() * 79);
-
     getRandomVideos().then(({ videos }) => setVideo(videos[randomNumber].video_files[0].link));
   }, []);
 
-  //   if (profile && user && videoURI) return <></>;
-  if (!profile && videoURI) return <></>;
+  if (!profile && !videoURI) return <></>;
 
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.header}>
-        <Header data={{ profile, user, videoURI }} />
-      </View>
-    </View>
+    <>
+      <Box alignItems={'center'} safeArea>
+        {/* Profile */}
+        <VStack space={5}>
+          <Center>
+            <Pressable onPress={() => {}}>
+              <Box>
+                <Image source={{ uri: profile?.picture?.large }} style={styles.profile_picture} />
+              </Box>
+            </Pressable>
+          </Center>
+
+          {/* Quotes */}
+
+          <Center>
+            <Text>Software Engineer at Facebook</Text>
+            <Text>"{user.quote}"</Text>
+          </Center>
+
+          {/* User name text */}
+          <Center>
+            <Text bold>
+              {profile.name.first} {profile.name.last}
+            </Text>
+            <Text>@{profile.login.username}</Text>
+          </Center>
+
+          {/* Divider */}
+          <Center>
+            <Box borderTopWidth={1} w={170} />
+          </Center>
+        </VStack>
+      </Box>
+    </>
   );
 };
