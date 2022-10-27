781b5909e0772548dec491d867e93e56ae362c67
Bryan E. Tejada <83300615+brodriguez-eb@users.noreply.github.com>
2022-10-26 22:31:16 -0400

    fixing build problems (still working..)

---
+++ app.config.js
@@ -0,0 +1,51 @@
+module.export = {
+  expo: {
+    name: "Observe",
+    slug: "Observe",
+    owner: "observe",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    plugins: [
+      "@react-native-firebase/app",
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            useFrameworks: "static",
+          },
+        },
+      ],
+    ],
+    assetBundlePatterns: ["**/*"],
+    // android: {
+    //   adaptiveIcon: {
+    //     foregroundImage: "./assets/adaptive-icon.png",
+    //     backgroundColor: "#FFFFFF",
+    //   },
+    //   googleServicesFile: "./google-services.json",
+    //   package: "com.observe.app",
+    // },
+    // ios: {
+    //   googleServicesFile: "./GoogleService-Info.plist",
+    //   bundleIdentifier: "com.observe.app",
+    // },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    // extra: {
+    //   eas: {
+    //     projectId: "9dcf67e9-bc5b-479c-9f71-69564a0952a7",
+    //   },
+    // },
+  },
+};
